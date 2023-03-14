1ab97a17361114de6a9b003bb5bdd509fcfd63b3
stasvitalich <stasvitalich1994@gmail.com>
2023-03-14 17:20:15 +0600

    Custom menu was created.

--- app/src/main/java/com/example/toolbar/MainActivity.kt
+++ app/src/main/java/com/example/toolbar/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.toolbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true) // Show arrow icon
+        supportActionBar?.title = "My app" // Cnange toolbar name
+    }
+
+    // Show custom toolbar with icons
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.mainmenu, menu)
+        return true
+    }
+
+    // Arrow icon onClick Listener
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) finish()
+        return true
     }
 }
